12504489dc0e0c24613c68f27676e9e4f62e2179
Aradi Patrik <aradipatrik@gmail.com>
2017-11-12 00:05:36 +0100

    rewrite html classes and use display flex instead of display grid (grid too buggy)

--- reservation.js
+++ reservation.js
@@ -1,21 +1,3 @@
 $(function() {
-  $(".date").datepicker({
-    dateFormat: 'yy-mm-dd',
-    firstDay: 1
-  });
-
-  $(document).on('click', '.date-input-group', function(e) {
-    var $me = $(this);
-    console.log($me);
-    $parent = $me.parents('.date-input-group');
-    $parent.toggleClass('open');
-  });
-
-
-  $(".date-input-group").on("change", function() {
-    var $me = $(this),
-      $selected = $me.val(),
-      $parent = $me.parents('.date');
-    $parent.find('.result').children('span').html($selected);
-  });
-});
+  $(".datepicker").datepicker();
+})
