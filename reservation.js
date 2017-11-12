03000777249b335b8a1967b94b60f9dbd2f492b8
Aradi Patrik <aradipatrik@gmail.com>
2017-11-12 01:01:12 +0100

    create submit button, fix datepicker problems on mobile

--- reservation.js
+++ reservation.js
@@ -1,3 +1,11 @@
 $(function() {
-  $(".datepicker").datepicker();
+  $(".datepicker").datepicker({
+    dateFormat: 'yy/mm/dd'
+  });
 })
+
+$(document).on('click', '.datepicker .input', function(e) {
+  var $me = $(this),
+    $parent = $me.parents('.date-picker');
+  $parent.toggleClass('open');
+});
